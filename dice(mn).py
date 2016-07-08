6a04dfc98863042586a3b7c4215658a9bc6f6244
jaishreeurs <jaishree.a.urs@gmail.com>
2016-07-08 14:14:21 -0400

    Add files via upload

---
+++ dice(mn).py
@@ -0,0 +1,30 @@
+import random
+total=0
+m=input("row:")
+n=input("column:")
+while total!=m*n:
+    dice=random.randrange(1,7)
+    print ("the die value is" ,dice)
+    if(total<m*n):
+        rollAgain="yes"
+        total=total+ dice
+        print(total)
+        x=total%n
+        y=total//m
+        if y%2==1:
+            x=(n-1)-x
+    print ("the position is", y,x)
+    
+         
+    while total>m*n:
+        print ("this die value is not acceptable")
+        total=total-dice
+        dice=random.randint(1,6)
+        print("the die value is",dice)
+        total=total+dice
+        print(total)
+        print ("the position is", y,x)
+        print("\n")
+if total==m*n:
+    print ("you've won")
+    print("the position is",y,x)
