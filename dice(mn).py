270541a30d46f1da252ee09cda9ca6ec7b04886d
jaishreeurs <jaishree.a.urs@gmail.com>
2016-07-11 10:09:52 -0400

    Update dice(mn).py

--- dice(mn).py
+++ dice(mn).py
@@ -6,25 +6,17 @@ while total!=m*n:
     dice=random.randrange(1,7)
     print ("the die value is" ,dice)
     if(total<m*n):
-        
-        total=total+ dice
-        print(total)
-        x=(total-1)%n
-        y=(total-1)//n
-        if y%2==1:
-            x=(n-1)-x
-    print ("the position is", y,x)
-    
-         
-    while total>m*n:
+    	total=total+ dice
+    	print(total)
+    if total>m*n:
+    	print("the die value is:",dice)
         print ("this die value is not acceptable")
         total=total-dice
-        dice=random.randint(1,6)
-        print("the die value is",dice)
-        total=total+dice
-        print(total)
-        print ("the position is", y,x)
-        print("\n")
+    x=(total-1)%n
+    y=(total-1)//n
+    if y%2==1:
+    	x=(n-1)-x
+    print ("the position is", y,x)
 if total==m*n:
     print ("you've won")
     print("the position is",y,x)
